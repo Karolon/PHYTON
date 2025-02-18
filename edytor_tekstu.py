7cdd2582a544f9dddfb9c094d0c48156b39ef35b
Karolon2 <109922689+Karolon@users.noreply.github.com>
2025-02-18 16:28:02 +0100

    Edytor textu update
    Support for sql

--- edytor_tekstu.py
+++ edytor_tekstu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import sqlite3 as sq
 
 #window options
 root = tk.Tk()
@@ -6,16 +7,34 @@ root.title('Edytor txtxtxtxttxtt')
 root.geometry('500x350')
 root.minsize(300,200)
     
-t = tk.Text(root,bg = "light cyan", font=20)
-t.place(x = 20, y =20, height = root.winfo_height()-40, width = root.winfo_width()-40)
-t.insert(tk.END, 'TESTTTTTTTTTTTTTTTTTTTTTS')
+txt = tk.Text(root,bg = "light cyan", font=20)
+txt.place(x = 20, y =20, height = root.winfo_height()-40, width = root.winfo_width()-40)
+txt.insert(tk.END, 'TESTTTTTTTTTTTTTTTTTTTTTS') 
 
 def NEW():
-  t.delete('1.0',tk.END)  
+  txt.delete('1.0',tk.END)  
   
 def test():
-  print(t.index())
-  
+  print(txt.index())
+
+def run_sql():
+  connect_sql = sq.connect('txt.db') 
+  cursor = connect_sql.cursor()
+
+  def execute_sql(s = str()):
+    try:
+      for cmd in s.split(';'):
+        cursor.execute(cmd)
+      connect_sql.commit()
+    except sq.OperationalError as err:
+      print(err)
+
+  if txt.tag_ranges('sel'):
+    execute_sql(txt.selection_get())
+  else:
+    execute_sql(txt.get('1.0',tk.END))
+
+
 #menu setup
 menubar = tk.Menu(root)
 root.config(menu=menubar)
@@ -24,13 +43,14 @@ menubar.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label = 'New', command = NEW)
 file_menu.add_command(label = 'Open', command = test)
 file_menu.add_command(label = 'Save', command = lambda : print(3))
-root.update_idletasks()
 
+menubar.add_command(label='SQL', command = run_sql)
+root.update_idletasks()
 
 
 
 while root.state() != 'closed':
   root.update()
   root.update_idletasks()
-  t.place(height = root.winfo_height()-40, width = root.winfo_width()-40)
+  txt.place(height = root.winfo_height()-40, width = root.winfo_width()-40)
   
