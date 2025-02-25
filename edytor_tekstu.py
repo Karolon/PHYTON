2559313ef1c07c3d8839d4735b60f138589ca315
Karolon2 <109922689+Karolon@users.noreply.github.com>
2025-02-25 16:30:57 +0100

    update

--- edytor_tekstu.py
+++ edytor_tekstu.py
@@ -11,6 +11,18 @@ txt = tk.Text(root,bg = "light cyan", font=20)
 txt.place(x = 20, y =20, height = root.winfo_height()-40, width = root.winfo_width()-40)
 txt.insert(tk.END, 'TESTTTTTTTTTTTTTTTTTTTTTS') 
 
+def pop_message(s = 'error'):
+  popup = tk.Tk()
+  popup.title('Wiadomość')
+  popup.minsize(100,100)
+  popup.update()
+  txt_mssg = tk.Text(popup,bg = "light cyan", font=10, )
+  txt_mssg.place(x = 2, y =2, height = popup.winfo_height()-60, width = popup.winfo_width()-4)
+  txt_mssg.insert(tk.END, s)
+  butt = tk.Button(popup, bg = "light cyan", font=30, text='OK', command=popup.destroy)
+  butt.place(x=20, y=popup.winfo_height()-50 )
+
+
 def NEW():
   txt.delete('1.0',tk.END)  
   
@@ -27,13 +39,18 @@ def run_sql():
         cursor.execute(cmd)
       connect_sql.commit()
     except sq.OperationalError as err:
-      print(err)
+      pop_message(err)
 
   if txt.tag_ranges('sel'):
     execute_sql(txt.selection_get())
   else:
     execute_sql(txt.get('1.0',tk.END))
 
+  message = cursor.fetchall()
+  if len(message) > 0:
+    pop_message(message)
+
+  connect_sql.commit()
 
 #menu setup
 menubar = tk.Menu(root)
